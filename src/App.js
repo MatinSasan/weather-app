176c3539623ddbffc3b2be6dfba89d286cca874f
Matin Sasanpour <matin.sasan@gmail.com>
2020-01-27 13:51:52 +0330

    css some bugs fixed | added more html elements | further styling css

--- src/App.js
+++ src/App.js
@@ -7,12 +7,53 @@ const api = {
   base: 'https://api.openweathermap.org/data/2.5'
 };
 
+const dateBuilder = dateItem => {
+  let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  let days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
+
+  let day = days[dateItem.getDay()];
+  let date = dateItem.getDate();
+  let month = months[dateItem.getMonth()];
+  let year = dateItem.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+};
+
 function App() {
   return (
     <div className='app'>
       <main>
         <div className='search-box'>
           <input type='text' className='search-bar' placeholder='Search...' />
+        </div>
+        <div className='location-box'>
+          <div className='location'>Tehran, Iran</div>
+          <div className='date'>{dateBuilder(new Date())}</div>
+        </div>
+        <div className='weather-box'>
+          <div className='temp'>20°C</div>
+          <div className='weather'>Sunny</div>
         </div>
       </main>
     </div>
