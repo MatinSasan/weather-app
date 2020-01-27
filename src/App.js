efe633bd49d23ab6aaf6e2dd1b3dda793e6ae5f2
Matin Sasanpour <matin.sasan@gmail.com>
2020-01-27 13:22:50 +0330

    added the base of the main and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { KEY } from './config';
+
+const api = {
+  key: KEY,
+  base: 'https://api.openweathermap.org/data/2.5'
+};
 
 function App() {
   return (
-    <div className='App'>
-      <h1>hello...</h1>
+    <div className='app'>
+      <main>
+        <div className='search-box'>
+          <input type='text' className='search-bar' placeholder='Search...' />
+        </div>
+      </main>
     </div>
   );
 }
