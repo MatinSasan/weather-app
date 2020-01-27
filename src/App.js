fc37235824fe133505bb01b8298a864576250182
Matin Sasanpour <matin.sasan@gmail.com>
2020-01-27 14:39:29 +0330

    conditional rendering added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import './App.css';
 import { KEY } from './config';
 
@@ -43,21 +43,39 @@ const dateBuilder = dateItem => {
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
+  const [alert, setAlert] = useState(null);
 
   const search = async e => {
-    if (e.key === 'Enter') {
-      const res = await fetch(
-        `${api.base}weather?q=${query}&units=metric&APPID=${api.key}`
-      );
-      const result = await res.json();
-      setWeather(result);
-      setQuery('');
-      console.log(result);
+    try {
+      if (e.key === 'Enter') {
+        const res = await fetch(
+          `${api.base}weather?q=${query}&units=metric&APPID=${api.key}`
+        );
+        const result = await res.json();
+        if (result.cod === '404') {
+          setAlert(result.message);
+          setTimeout(() => {
+            setAlert(null);
+          }, 4000);
+        }
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+      }
+    } catch (err) {
+      console.error(err);
     }
   };
 
   return (
-    <div className='app'>
+    <div
+      className={
+        typeof weather.main !== 'undefined'
+          ? weather.main.temp > 18
+            ? 'app warm'
+            : 'app'
+          : 'app'
+      }>
       <main>
         <div className='search-box'>
           <input
@@ -69,14 +87,31 @@ function App() {
             onKeyPress={search}
           />
         </div>
-        <div className='location-box'>
-          <div className='location'>Tehran, Iran</div>
-          <div className='date'>{dateBuilder(new Date())}</div>
-        </div>
-        <div className='weather-box'>
-          <div className='temp'>20°C</div>
-          <div className='weather'>Sunny</div>
-        </div>
+        {typeof weather.main !== 'undefined' ? (
+          <Fragment>
+            <div className='location-box'>
+              <div className='location'>
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className='date'>{dateBuilder(new Date())}</div>
+            </div>
+            <div className='weather-box'>
+              <div className='temp'>{Math.round(weather.main.temp)}°c</div>
+              <div className='weather'>{weather.weather[0].main}</div>
+            </div>
+          </Fragment>
+        ) : (
+          <Fragment>
+            {alert !== null ? (
+              <div className='location-box'>
+                <div className='location'>{alert}</div>
+              </div>
+            ) : (
+              ''
+            )}
+            <div>{''}</div>
+          </Fragment>
+        )}
       </main>
     </div>
   );
